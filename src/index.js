f5b95ab8b8d8883b0b1b6e4191de7958766a6e4f
danadrol96 <parkdk96@gmail.com>
2020-10-15 01:22:05 -0700

    creating indexjs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,122 @@
 STATE = {
-    bitz: [],
-    displayBitz: [],
-    filter: "All",
+    bitz: [
+        {
+        "bitzId": "1",
+        "name": "Game 1",
+        "game": true,
+        "tags": [1,2,3]
+        },
+        {
+        "bitzId": "2",
+        "name": "Trinket 1",
+        "game": false,
+        "tags": [5]
+        },
+        {
+        "bitzId": "3",
+        "name": "Trinket 2",
+        "game": false,
+        "tags": [2,4]
+        },
+        {
+        "bitzId": "4",
+        "name": "Game2",
+        "game": true,
+        "tags": [1,3,5]
+        },
+        {
+        "bitzId": "5",
+        "name": "Game 3",
+        "game": true,
+        "tags": [2,4,5]
+        },
+        {
+        "bitzId": "6",
+        "name": "Game 4",
+        "game": true,
+        "tags": [1,2,3,4,5]
+        },
+        {
+        "bitzId": "7",
+        "name": "Trinket 3",
+        "game": false,
+        "tags": [3,4]
+        },
+        {
+        "bitzId": "8",
+        "name": "Game 5",
+        "game": true,
+        "tags": [1,4,5]
+        }
+    ],
+    displayBitz: [
+        {
+            "bitzId": "1",
+            "name": "Game 1",
+            "game": true,
+            "tags": [1,2,3]
+            },
+            {
+            "bitzId": "2",
+            "name": "Trinket 1",
+            "game": false,
+            "tags": [5]
+            },
+            {
+            "bitzId": "3",
+            "name": "Trinket 2",
+            "game": false,
+            "tags": [2,4]
+            },
+            {
+            "bitzId": "4",
+            "name": "Game2",
+            "game": true,
+            "tags": [1,3,5]
+            },
+            {
+            "bitzId": "5",
+            "name": "Game 3",
+            "game": true,
+            "tags": [2,4,5]
+            },
+            {
+            "bitzId": "6",
+            "name": "Game 4",
+            "game": true,
+            "tags": [1,2,3,4,5]
+            },
+            {
+            "bitzId": "7",
+            "name": "Trinket 3",
+            "game": false,
+            "tags": [3,4]
+            },
+            {
+            "bitzId": "8",
+            "name": "Game 5",
+            "game": true,
+            "tags": [1,4,5]
+            }
+    ],
+    filterType: "All",
+    filterTag: 0,
     sort: "Newest"
 }
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    let 
+    let alph = document.getElementById("alph")
+    let newest = document.getElementById("new")
+    let oldest = document.getElementById("old")
+    let all = document.getElementById("all")
+    let games = document.getElementById("games")
+    let trinkets = document.getElementById("trinkets")
+    let one = document.getElementById("1")
+    let two = document.getElementById("2")
+    let three = document.getElementById("3")
+    let four = document.getElementById("4")
+    let five = document.getElementById("5")
 })
 
